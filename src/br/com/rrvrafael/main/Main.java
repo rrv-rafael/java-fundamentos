f4bfb38913be23070a54eddba46c36f812d16759
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2022-05-02 22:31:19 -0300

    pequenas alterações

--- src/br/com/rrvrafael/main/Main.java
+++ src/br/com/rrvrafael/main/Main.java
@@ -1,7 +1,33 @@
 package br.com.rrvrafael.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+
+        int numero1, numero2;
+        char operador;
+
+        System.out.println("Informe o 1o número:");
+        numero1 = scan.nextInt();
+
+        System.out.println("Informe o 2o número:");
+        numero2 = scan.nextInt();
+
+        System.out.println("Informe o operador matemático:");
+        operador = scan.next().charAt(0);
+
+        /*System.out.println("Você quer fazer a operação: " + numero1 + operador + numero2);*/
+        System.out.printf("\nVocê quer fazer a operação: %d %c %d\n\n", numero1, operador, numero2);
+        switch (operador) {
+            case '+' -> System.out.println("Resultado da operação: " + (numero1 + numero2));
+            case '-' -> System.out.println("Resultado da operação: " + (numero1 - numero2));
+            case '*' -> System.out.println("Resultado da operação: " + (numero1 * numero2));
+            case '/' -> System.out.println("Resultado da operação: " + (numero1 / numero2));
+            default -> System.out.println("Operador inválido!");
+        }
+
+        scan.close();
     }
 }
